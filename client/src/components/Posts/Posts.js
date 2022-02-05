3a42d1b80d64403884fa72663fccf7f1eb0f5d2e
Alan Zhang <alanzhang864@gmail.com>
2022-02-04 17:53:55 -0800

    display posts completed

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,6 +3,8 @@ import Post from './Post/Post'
 import useStyles from './styles'
 import { useSelector } from "react-redux";
 
+import {Grid, CircularProgress} from '@material-ui/core'
+
 const Posts =() => {
     const posts = useSelector((state)=>state.posts)
  
@@ -10,12 +12,20 @@ const Posts =() => {
 
     console.log(posts)
     return (
-        <div>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </div>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className = {classes.container} container alignItems = "stretch" spacing = {3}>
+                {
+                    posts.map((post) => (
+                        <Grid  item xs = {12} sm={6}>
+                            <Post key = {post.id} post = {post} />
+                        </Grid>
+
+                    ))
+                }
+            </Grid>
+        )
     )
 }
+    
 
 export default Posts 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,8 +5,6 @@ export const getPosts = async (req, res)=> {
     try {
         const postMessages = await PostMessage.find();
 
-        console.log(postMessages)
-
         res.status(200).json(postMessages)
     } catch (error) {
         res.status(404).json({message: error.message})
@@ -17,6 +15,7 @@ export const createPost = async (req, res) => {
     const post = req.body;
 
     const newPost = new PostMessage(post);
+    console.log(newPost)
 
     try {
         await newPost.save()
