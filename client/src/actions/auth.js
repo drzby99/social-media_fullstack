0b966516a37b9659d6024384610779dc787bf304
az <alanzhang864@gmail.com>
2022-02-15 14:44:17 -0800

    jwt kinda works now

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,25 +1,26 @@
 import { AUTH } from '../constants/actions';
 import * as api from '../api/index.js';
 
-export const signin = (formData, router) => async (dispatch) => {
+
+export const signin = (formData, history) => async (dispatch) => {
   try {
     const { data } = await api.signIn(formData);
 
     dispatch({ type: AUTH, data });
 
-    router.push('/');
+    history('/');
   } catch (error) {
     console.log(error);
   }
 };
 
-export const signup = (formData, router) => async (dispatch) => {
+export const signup = (formData, history) => async (dispatch) => {
   try {
     const { data } = await api.signUp(formData);
 
     dispatch({ type: AUTH, data });
 
-    router.push('/');
+    history('/');
   } catch (error) {
     console.log(error);
   }
